04e6a168c21db71d6a16b6a9794be3865e69915e
nehalmeister@gmail.com <nehalmeister@gmail.com>
2020-05-08 03:53:07 -0700

    more doc

--- test/ccore_test_api.py
+++ test/ccore_test_api.py
@@ -1,71 +1,77 @@
-import requests 
-import pdb
-import random
-import string 
-import sys
-url = "http://zonkatronic.com:4000/api/"
-url = "http://localhost:4000/api/"
-headers = {"Content-Type":"application/json"}
+"""
+   Hello
+"""
+
+import requests
+URL = "http://localhost:4000/api/"
+headers = {"Content-Type": "application/json"}
 
 
-def create_user(email,password):
-   
+def create_user(email, password):
     """
     curl -H "Content-Type: application/json" -X POST \
     -d '{"user":{"email":"some@email.com","password":"some password"}}' \
      http://localhost:4000/api/users
     """
-   
-    data={"user":{"email":email,"password":password}} 
-    create_user_url = url+"/users/create"
-    response = requests.post(create_user_url,headers=headers,json=data)
+
+    data = {"user": {"email": email, "password": password}}
+    create_user_url = URL + "/users/create"
+    response = requests.post(create_user_url, headers=headers, json=data)
     return response
 
-def auth(email,password):
-    data = {"email":email,"password":password}
-    sign_in_url=url+"users/sign_in"
-    response = requests.post(sign_in_url,headers=headers,json=data)
+
+def sign_in(email, password):
+    """ Sign in user
+    Arguments:
+        email -- account name
+        password` - password
+    Return:
+       Session cookie
+    """
+    data = {"email": email, "password": password}
+    sign_in_url = URL + "users/sign_in"
+    try:
+        response = requests.post(sign_in_url, headers=headers, json=data)
+    except:
+        return {"Error": "error"}
     cookiejar = response.cookies
     return cookiejar
 
-def sat_spawn(name,cookiejar):
-    url_spawn = url + "spawn"
-    spawn_data = {"name": name,"extra_channels":["topic:missileroom"],"visible":0}
-    response = requests.post(url_spawn, headers=headers,json=spawn_data,cookies=cookiejar)
-    print(response)
 
-def get_graph(name,cookiejar):
-    url_spawn = url + "spawn"
-   
+def spawn(proc_type, name, extra_channels, visible, cookiejar):
+    """Spawn a process
+    Arguments:
+       proc_type - type of process to start
+       name - name of the process
+       extra_channels - additional channels to route process data
+       visible - nothing
+       cookiejar  - session id
+    """
+    url_spawn = URL + "spawn"
+    spawn_data = {
+        "proc_type": proc_type,
+        "name": name,
+        "extra_channels": extra_channels,
+        "visible": visible
+    }
+    try:
+        response = requests.post(url_spawn,
+                                 headers=headers,
+                                 json=spawn_data,
+                                 cookies=cookiejar)
+        return response
+    except requests.exceptions.RequestException as e_e:
+        return {"Error": e_e}
 
 
-def missile_spawn():
-    url = "http://localhost:4000/api/"
-    missile_spawn = url + "missile"
-    url_data = {"missile_name": "m1", "cmd": "launch"}
-    response = r.post(missile_spawn, data=url_data)
-    print(response.content)
-    for i in range(0, 100):
-        pdb.set_trace()
-        url_data = {"missile_name": "m1",
-                    "cmd": "add_position", "position": str(i)}
-        response = r.post(missile_spawn, data=url_data)
-        print(response.content)
+def connect_procs(proc_a, proc_b):
+    """ Connect two procs"""
+    return "Foo"
 
-#letter = random.choice(string.ascii_lowercase)
-#letter2 = random.choice(string.ascii_lowercase)
-#user = "xx"+letter+letter2
-#user = "nehal.desaix@gmail.com"
-#response = create_user("nehal.desaix@aero.org","foobar") 
-pdb.set_trace()
-#cookie = auth(user,"foobar") 
-#print(response) 
-cookie = auth("nehal.desaix@aero.org","foobar")
-pdb.set_trace()
-name="GOES 15"
-#for name in ['GOES 15', 'GOES 13', 'ASIASAT 9','41937','39120']:
-sat_spawn(name,cookie)
+
+#cookie = auth("nehal.desaix@aero.org", "foobar")
+# spawn(cookie)
 #
-#print(cookie)
-#missile_spawn()
-#sat_spawn()
+# print(cookie)
+# missile_spawn()
+# sat_spawn()
