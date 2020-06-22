0d1212778de5143b1ec76da037b1d62be89eda54
nehalmeister@gmail.com <nehalmeister@gmail.com>
2020-06-21 22:10:24 -0700

    removing extra codey

--- test/ccore_test_gs.py
+++
@@ -1,8 +0,0 @@
-import ccore_test_api as cta
-import pdb
-
-user_cookie = cta.sign_in("nehal.desaix@aero.org", "foobar")
-spawn_handle_45555 = cta.spawn_proc(user_cookie, "generic", "45555", [], 0)
-pdb.set_trace()
-cta.info_proc(user_cookie,spawn_handle_45555) 
-
